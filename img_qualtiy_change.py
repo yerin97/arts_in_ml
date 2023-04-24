03ba897890210dd2f185ec1882ca5a918900ec14
yerin97 <92047632+yerin97@users.noreply.github.com>
2023-04-23 23:06:03 -0400

    Add files via upload

---
+++ img_qualtiy_change.py
@@ -0,0 +1,28 @@
+def img_quality(source,dest):
+    source="/Users/yerin/examples"
+    dest="/Users/yerin/cropped/fakes000160_bigger"
+    max_images=500
+    transform="center-crop"
+    resolution=(512,512)
+
+    img = Image.open(source)
+
+    # Resize the image to 512x512 with Lanczos filter
+    resized_img = img.resize((1024, 1024), Image.LANCZOS)
+
+    # Apply post-processing filters to reduce loss of quality
+    resized_img = resized_img.filter(ImageFilter.SHARPEN)
+    resized_img = resized_img.filter(ImageFilter.SHARPEN)
+    resized_img = resized_img.filter(ImageFilter.SHARPEN)
+    resized_img = resized_img.filter(ImageFilter.SHARPEN)
+    resized_img = resized_img.filter(ImageFilter.SHARPEN)
+    resized_img = resized_img.filter(ImageFilter.SMOOTH_MORE)
+
+
+    # Save the resized image
+    resized_img.save(dest)
+
+if __name__ == '__main__':
+  source = sys.argv[1]
+  dest = sys.argv[2]
+  img_quality(source,dest)
